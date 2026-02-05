c9dd642eb20a29c0fb55a64226f8f9b61f9b2e14
Milan Saxena <46219948+MilanS56@users.noreply.github.com>
2026-02-05 13:51:10 +0530

    changed the port in CORS

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const pronounce = require("./routes/pronounce");
+app.use(express.json());
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST"]
+  origin: [
+    "https://pronunciation-engine-frontend.vercel.app",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
 }));
 
 app.use("/api/pronounce", pronounce);

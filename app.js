b1dc8930dda98cf30c0ba6b270897822beb7ad78
Milan Saxena <46219948+MilanS56@users.noreply.github.com>
2026-02-05 13:37:45 +0530

    updated App.js port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const pronounce = require("./routes/pronounce");
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST"]
@@ -11,4 +11,6 @@ app.use(cors({
 
 app.use("/api/pronounce", pronounce);
 
-app.listen(PORT, () => console.log("Server Running"));
+app.listen(PORT, () => {
+  console.log("AI server running on", PORT);
+});
